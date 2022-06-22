ae41767812778ac50121e54039e3527ba594f05a
Lucas Rezende <66080424+Lucas-Rezende@users.noreply.github.com>
2022-06-22 15:30:05 -0300

    add SaidaDeDados
    Código que faz uso dos comandos printf e \n.

---
+++ 2-SaidaDeDados/teste-de-saida.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("O comando printf() possibilita a saída de dados.");
+    printf("\n"); //comando para quebra de linha    
+    printf("O comando '\n' permite a quebra de linha.");
+    return 0;
+}
+ 
