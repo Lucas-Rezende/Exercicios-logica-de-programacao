a2515a47e808e667fef2f44e8eeb80dd41d71423
Lucas Rezende <66080424+Lucas-Rezende@users.noreply.github.com>
2022-06-23 09:59:36 -0300

    remove \n

--- 4-EntradaDeDados/teste-de-entrada.c
+++ 4-EntradaDeDados/teste-de-entrada.c
@@ -5,6 +5,6 @@ int main()
     int v1;
     printf("\nDigite um numero: ");
     scanf("%i", &v1);
-    printf("\nO numero que voce digitou foi: %i", v1, "\n");
+    printf("\nO numero que voce digitou foi: %i", v1);
     return 0;
 }
