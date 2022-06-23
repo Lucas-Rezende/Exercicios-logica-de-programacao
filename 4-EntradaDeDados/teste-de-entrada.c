44002001c6bce878308a4294cc23cb9aed708780
Lucas Rezende <66080424+Lucas-Rezende@users.noreply.github.com>
2022-06-23 09:34:50 -0300

    add teste-de-entrada
    Código com uso prático da função scanf.

---
+++ 4-EntradaDeDados/teste-de-entrada.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() 
+{
+    int v1;
+    printf("\nDigite um numero: ");
+    scanf("%i", &v1);
+    printf("\nO numero que voce digitou foi: %i", v1, "\n");
+    return 0;
+}
