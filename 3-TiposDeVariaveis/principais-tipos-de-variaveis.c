21b2a6976f334d3b078f7b55f18dc4382a6b3337
Lucas Rezende <66080424+Lucas-Rezende@users.noreply.github.com>
2022-06-23 08:53:30 -0300

    add principais-tipos-de-variaveis
    Uso prático de uma variável e a imprimindo com printf.

---
+++ 3-TiposDeVariaveis/principais-tipos-de-variaveis.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //char   | letras e símbolos:   'a',  'b', 'H', '^', '*','1','0'
+    //int    | de -32767 até 32767 (apenas números inteiros)
+    //float  | de -3.4 x 1038 até +3.4 x 10+38 com até 6 dígitos de precisão (Números reais)
+    //double | de -1.7 x 10308 até +1.7 x 10+308 com até 10 dígitos de precisão (Números reais)
+
+    int idade = 33;
+
+    printf("%d", idade);
+
+    return 0;
+}
+
