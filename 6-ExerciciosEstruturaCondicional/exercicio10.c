15be9d5bb7f53db44ec455723f6886087c5502fb
Lucas Rezende <66080424+Lucas-Rezende@users.noreply.github.com>
2022-07-07 16:09:47 -0300

    edit exercicio 10

--- 6-ExerciciosEstruturaCondicional/exercicio10.c
+++ 6-ExerciciosEstruturaCondicional/exercicio10.c
@@ -4,8 +4,22 @@ qualquer ordem.*/
 
 #include <stdio.h>
 
-int main(){
+int main() {
     int num1;
     int num2;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &num1);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &num2);
+
+    if (num1%num2==0 || num2%num1==0)
+    {
+        printf("Sao multiplos");
+    } else
+    {
+        printf("Nao sao multiplos");
+    }
+
     return 0;
 }
